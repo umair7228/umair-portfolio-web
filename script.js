cb11a66fa595f038f123086d0ef5ce2ebbdf1222
umair7228 <umairnawan7228@gmail.com>
2024-07-11 12:15:47 +0500

    Portfolio Web

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed (".text", {
+    strings: ["Python Developer", "TypeScript Developer", "HTML | CSS Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 650,
+    loop: true
+}) 
